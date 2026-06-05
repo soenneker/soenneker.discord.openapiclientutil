648c250b7d2ef9cdd5e5bae1614495e4e72570a8
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:08 -0500

    xml commenting

--- src/Soenneker.Discord.OpenApiClientUtil/Abstract/IDiscordOpenApiClientUtil.cs
+++ src/Soenneker.Discord.OpenApiClientUtil/Abstract/IDiscordOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Discord.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IDiscordOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<DiscordOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
